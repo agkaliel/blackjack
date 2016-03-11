fed864c583ede40d14fa39637c21135bd3d8dc8c
agkaliel <agkaliel@gmail.com>
2016-03-11 16:25:13 -0700

    first commit

---
+++ agents.py
@@ -0,0 +1,229 @@
+import blackjack
+from tabulate import tabulate
+import copy
+import re
+
+
+def probBust(hand, target):
+    maximum = target-hand
+
+    #if target == hand, i.e. we are already at 21, then ProbBust = 1
+    if maximum == 0:
+        return 1
+    #if target - hand >= 10 i.e. we are at 11 or lower, there is no possible way to bust.  
+    if maximum >= 10:
+        return 0
+    #if the maximum is between 1 and 9, the probability of busting is the number of possible values greater than maximum (13 - maximum) out of thirteen
+    return (13-maximum)/13
+
+
+#Assumptions:
+#Game is played with 8 decks
+#Assume we start with a fresh 8 decks after each hand
+
+
+def agentTest(agent):
+    frequencies = []
+    probabilities = []
+    #Populate frequences with 12 0's, as there are 12 values to keep track of between 11 and 2s
+    for i in range(12):
+        frequencies.append(0)
+        probabilities.append(0)
+    sampleSize = 100
+
+    #Play blackjack through "sampleSize" games
+    for i in range(sampleSize):
+        game = blackjack.BlackJack(8)
+        game.start_game()
+        while game.state.terminate == 0:
+            action = game.act(agent)
+        final = blackjack.BlackJack.calc(game.state.player_hand)
+        if final >= 22:
+            frequencies[11] += 1
+        else:
+            frequencies[final - 11] += 1
+
+    for i in range(12):
+        probabilities[i] = frequencies[i]/sampleSize
+
+    return probabilities
+
+
+#The function dealer test is basically a modified version of agentTest()
+#The list startValues contains a list of lists, where the first element 
+#refers to the dealer starting with an ace, the second element to the dealer
+#starting with a 2 ect.  There are 10 lists in start value.  Each list in start
+#values contains the frequencies with which the dealer ended on each final hand
+#value.  So in the end, we get a 10*12 grid.  We convert these frequencies to
+#probabilties at the end.
+def dealerTest(agent):
+    startValues = []
+    for i in range(10):
+        startValues.append([])
+
+
+    #Populate each the 10 lists in startValues with 12 0's, as there are 12 "finish" values to keep track of between 11 and 22
+    for startValue in startValues:
+        for i in range(12):
+            startValue.append(0)
+    sampleSize = 100
+
+
+    #Play blackjack through "sampleSize" games
+    for i in range(sampleSize):
+        game = blackjack.BlackJack(8)
+        game.start_game()
+
+        while game.state.terminate == 0:
+            first = game.state.player_hand[0][0]
+            if game.state.player_hand[0][1] == '0':
+                first = first + '0'
+            if first == 'A':
+                first = 1
+            elif first == 'J' or first == 'Q' or first == 'K':
+                first = 10
+            else:
+                first = int(float(first))
+
+            action = game.act(agent)
+
+        final = blackjack.BlackJack.calc(game.state.player_hand)
+        if final >= 22:
+            startValues[first-1][11] += 1
+        else:
+            startValues[first-1][final - 11] += 1
+
+    probabilities = copy.deepcopy(startValues)
+    for row in range(10):
+        for col in range(12):
+            probabilities[row][col] = (startValues[row][col] / sum(startValues[row]))
+    print(tabulate(startValues))
+    text_file = open("outputbetter.txt", "w")
+    text_file2 = open("outputbetterraw.txt", "w")
+    text_file.write(tabulate(probabilities))
+    text_file.write(tabulate(startValues))
+    text_file2.write(str(probabilities))
+    text_file.close()
+    text_file2.close()
+    print(tabulate(probabilities))
+
+
+
+def prepSophAgent():
+    dealerTestOutput = open('output.txt', 'r')
+    dealerProbs = []
+    #Get the data from the output file from the dealerTest() function
+    for line in dealerTestOutput:
+        inner_list = [elt.strip() for elt in line.split('  ')]
+        dealerProbs.append(inner_list)
+
+    agentProbs = []
+
+    #want win/tie probabilities for hit/stand for 12 through 21 (10 values)
+    #each row will refer to the dealers start card, each column to the players hand value
+    for row in range(10):
+        agentProbs.append([])
+        for col in range(10):
+            #The first element of each tuple (actually a list so I can change assignments) corresponds to the
+            #probability of a win/tie if the player hits,
+            #and the second element of each tuple corresponds to the probability of a win/tie if the player stands,
+            #corresponding to the dealer card and player card at that location.
+            agentProbs[row].append([0,0])
+
+    #If we are at 21, we always want to stay
+    for rowNum, row in enumerate(agentProbs):
+        row[9][1] = 1
+        #filling in the "stay" probabilities
+        for i in reversed(range(0,9)):
+            #probWinStay is the probability that the value we currently have is good enough to win or tie
+            #i.e. the probability that we will win or tie with a stay
+            probWinStay = 0
+            #0 in the dealerProbs list corresponds to the dealer finishing with 17
+            #0 in the agentProbs list corresponds to the action with a hand value of 12
+            #we want to include ties, so this means an offset of 4
+            for dealerFinish in range(i-4):
+                probWinStay += float(dealerProbs[rowNum][dealerFinish])
+            #add the probability of the dealer busting
+            probWinStay += float(dealerProbs[rowNum][5])
+            row[i][1] = round(probWinStay,5)
+        #filling in the "hit" probabilities
+        for i in reversed(range(0,9)):
+            probWinHit = 0
+            for nextPossible in range(i+1,10):
+                probWinHit += max(row[nextPossible])/13
+            row[i][0]=round(probWinHit,5)
+
+    #Now we make a table for the actual decisions we will make
+    strategy = []
+    for row in range(10):
+        strategy.append([])
+        for col in range(10):
+            strategy[row].append([])
+            if agentProbs[row][col][0] > agentProbs[row][col][1]:
+                strategy[row][col] = 'H'
+            else:
+                strategy[row][col] = 'S'
+
+
+
+
+    output = open('probabilityTable.txt', 'w')
+    output.write(tabulate(agentProbs))
+    output2 = open('agentStrategy.txt', 'w')
+    output2.write(tabulate(strategy))
+
+
+
+
+       
+
+
+class ThoughtlessAgent:
+    def __init__(self, actions=('H', 'S', 'D')):
+        self.actions = actions
+    
+    def act(self, state):
+        return self.actions[0]
+    
+class BasicAgent:
+    def act(self, state):
+        if blackjack.BlackJack.calc(state.player_hand) > 16:
+            return 'S'
+        else:
+            return 'H'
+
+class SophAgent:
+    strategyFile = open('agentStrategy.txt', 'r')
+    strategy = []
+    #Get the data from the output file from the dealerTest() function
+    for line in strategyFile:
+        inner_list = [elt.strip() for elt in line.split('  ')]
+        strategy.append(inner_list)
+    strategy = strategy[1:-1]
+
+    def act(self, state):
+        dealer = blackjack.BlackJack.calc(state.dealer_hand)
+        player = blackjack.BlackJack.calc(state.player_hand)
+        if dealer == 11:
+            dealer = 1
+        if player >= 12 and player <= 21:
+            move = self.strategy[dealer - 1][player - 12]
+            return move
+        else:
+            return 'H'
+
+
+class CommandLineAgent:
+    interactive = True
+    
+    def act(self, state):
+        """
+        Display the game state information and prompt for action choice
+        """
+        return input("Choose an action [H]it, [S]tand, [D]ouble:").upper()
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    agent = BasicAgent()
+    print(prepSophAgent())
---
+++ blackjack.py
@@ -0,0 +1,274 @@
+"""Blackjack dealer and experiment runner.
+
+Usage: 
+    blackjack.py [-vd] [--hands=N] [--decks=M][--seed=S][--agent=AGENT]
+    
+
+Options:
+    -h --help
+    -v              Verbose mode, show action output messages.
+    -d              Debug mode, execute docstring tests.
+    --hands=N       Number of hands to play [default: 100]
+    --decks=M       Number of decks to shuffle (reshuffles once half the cards have been played) [default: 8]
+    --agent=AGENT   Name of python agent class [default: BasicAgent]
+    --seed=S        Number to seed the random generator with.
+"""
+
+import random
+import agents
+from docopt import docopt
+from datetime import datetime
+import time
+
+class GameState:
+    def __init__(self, num_decks=2):
+        self.dealer_hand = []
+        self.player_hand = []
+        self.win_total = 0
+        self.terminate = 0
+        self.num_decks = num_decks  
+    
+    def reset(self):
+        self.dealer_hand = []
+        self.player_hand = []
+        self.win_total = 0
+        self.terminate = 0
+
+SUITS = ['S', 'C', 'H', 'D']
+FACE_CARDS = ['J', 'Q', 'K']
+FACES = ['A']+[str(i) for i in range(2, 11)]+FACE_CARDS
+STD_DECK = ["{}{}".format(f, s) for s in SUITS for f in FACES]
+
+class Deck:
+    def __init__(self, num_decks=1):
+        self.num_decks = num_decks
+        self.halfway = num_decks * 26
+        self.reset()
+    
+    def reset(self):
+        self.deck = [c for c in STD_DECK*self.num_decks]
+    
+    def shuffle(self):
+        random.shuffle(self.deck)
+        
+    def deal(self, num_cards=1, num_players=1):
+        """
+        Deal out the specified number of cards to the specified number of players.
+        Returns a tuple of tuples and modifies the deck.
+        
+        >>> d = Deck()
+        >>> d.deal()
+        [['KD']]
+        >>> d.deal(2)
+        [['QD', 'JD']]
+        >>> d.deal(num_players=3)
+        [['10D'], ['9D'], ['8D']]
+        >>> d.deal(num_players=3, num_cards=2)
+        [['7D', '6D'], ['5D', '4D'], ['3D', '2D']]
+        """
+        if len(self.deck) < self.halfway:
+            self.reset()
+            self.shuffle()
+        return [[self.deck.pop() for _ in range(num_cards)] for _ in range(num_players)]
+
+class BlackJack:
+    actions = ('H', 'S', 'D')
+
+    @classmethod    
+    def calc(self, hand):
+        """
+        calculate the value of the given hand
+        >>> BlackJack.calc(['AS', '10D'])
+        21
+        >>> BlackJack.calc(['AS', '10D', '3H'])
+        14
+        >>> BlackJack.calc(['AS', '7D', '3H'])
+        21
+        >>> BlackJack.calc(['AS', '6D', '3H'])
+        20
+        >>> BlackJack.calc(['2S', '3D'])
+        5
+        >>> BlackJack.calc(['8C', '7H', '9D'])
+        24
+        """
+        total = 0
+        num_aces = 0
+        for c in hand:
+            if c[0] == 'A':
+                num_aces += 1
+            elif c[0] in FACE_CARDS or c[0] == '1':
+                total += 10
+            else:
+                total += int(c[0])
+        # if total is greater than 10 the aces must count as 1s
+        if total > 10 or num_aces == 0:
+            return total + num_aces
+        else:
+            return total + 11 + (num_aces-1)    
+        
+
+    def __init__(self, num_decks=2):
+        """
+        Set up the decks and shuffle the cards
+        """
+        self.state = GameState(num_decks)
+        self.dealer_card = ''
+        self.deck = Deck(num_decks=self.state.num_decks)
+    
+    def display_hand(self, name="Player", cards=None):
+        if cards == None:
+            if name=="Player":
+                cards = self.state.player_hand
+            else:
+                cards = self.state.dealer_hand
+            
+        print("{} has {} for total of {}.".format(name, 
+                                                  tuple(cards), 
+                                                  self.calc(cards)))
+        
+    def start_game(self):
+        # deal cards
+        self.state.reset()
+        self.deck.shuffle()
+        (self.state.dealer_hand, self.state.player_hand) = self.deck.deal(num_cards=2, num_players=2)
+        # hide one dealer card
+        self.dealer_card = self.state.dealer_hand.pop()
+        self.state.win_total = 1        
+    
+    def act(self, agent):
+        """
+        Request an action from an agent object, update game state accordingly
+        """
+        choice = agent.act(self.state)
+        if choice == 'H' or choice == 'D':
+            self.state.player_hand.extend(self.deck.deal()[0])
+            if choice == 'D':
+                self.state.win_total *= 2
+        if choice == 'S' or choice == 'D':
+            self.state.terminate = 1
+        elif choice != 'H':
+            print("Sorry, invalid action {}.".format(choice))
+        
+        if self.calc(self.state.player_hand) > 21:
+            self.state.terminate = -1
+        return choice
+
+ 
+    def final(self):
+        self.state.dealer_hand.append(self.dealer_card)
+        p = self.calc(self.state.player_hand)
+        
+        # check if the player lost already
+        if p > 21:
+            self.state.terminate = -1
+            return self.state.terminate * self.state.win_total
+
+        # otherwise play out the dealer
+        while self.calc(self.state.dealer_hand) < 17:
+            self.state.dealer_hand.extend(self.deck.deal()[0])
+        self.state.terminate = 1
+
+        d = self.calc(self.state.dealer_hand)
+    
+        # see how the dealer and player compare
+        if d > 21 or p > d:
+            self.state.terminate = 1
+        elif p==d:
+            self.state.win_total = 0
+        else:
+            self.state.terminate = -1
+        return self.state.win_total * self.state.terminate
+
+def playAgain():
+    choice = input("Play again? y/n:")
+    if choice and choice[0].lower() == 'y':
+        print("Hooray!")
+        return True
+    else:
+        print("TTYL")
+        return False
+            
+def main(num_hands=10, num_decks=8, verbose=True, agent_class="CommandLineAgent", seed=None):
+    # double-check verbosity, if we're playing on the command line we should probably tell the player what's going on
+    if agent_class=="CommandLineAgent":
+        verbose = True
+
+    # proper randomization
+    try:
+        seed = int(seed)
+    except TypeError:
+        seed = int(time.mktime(datetime.now().timetuple()))
+
+    if verbose:
+        print("Starting with seed {}".format(seed))
+    random.seed(seed)
+    try:
+        num_decks = int(num_decks)
+    except:
+        num_decks = 8
+    try:
+        num_hands = int(num_hands)
+    except:
+        num_hands = 10
+        
+    game = BlackJack(num_decks)
+    agent = getattr(agents, agent_class)()
+    total = 0
+    num_wins = 0
+    num_losses = 0
+    for hand in range(num_hands):
+        game.start_game()
+        if verbose:
+            game.display_hand()
+            game.display_hand("Dealer")
+        
+        while game.state.terminate == 0:
+            action = game.act(agent)
+            if verbose:
+                print("Player chose {}".format(action))
+                game.display_hand()
+                
+        p = game.calc(game.state.player_hand)
+        if verbose:
+            if p > 21:
+                print("Player went bust.")                
+            else:
+                print("Player stays at {}".format(game.calc(game.state.player_hand)))
+        result = game.final()
+        if verbose:
+            game.display_hand("Dealer")
+
+        if result > 0:
+            if verbose:
+                print("--WIN--")
+            num_wins += 1
+        elif result < 0:
+            if verbose:
+                print("--lose--")
+            num_losses += 1
+        elif verbose:
+            print("--Tie--")
+        total += result
+        if hasattr(agent, 'interactive'):
+            if not playAgain():
+                break
+        if verbose:
+            print("Done game {} at {}.".format(hand, result))
+    num_ties = hand + 1 - num_wins - num_losses
+    print("Total winnings: {} over {} games.\n with {} wins, {} losses, {} ties.".format(total, 
+                                                                                     hand + 1,
+                                                                                     num_wins,
+                                                                                     num_losses,
+                                                                                     num_ties))
+    
+    
+if __name__ == '__main__':
+    options = docopt(__doc__)
+    if (options['-d']):
+        import doctest
+        doctest.testmod()
+    main(verbose=options['-v'], 
+         num_hands=options['--hands'], 
+         agent_class=options['--agent'], 
+         num_decks=options['--decks'],
+         seed=options['--seed'])
